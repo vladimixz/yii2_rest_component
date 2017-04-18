b0e7e6506ba8953a185774a698eb4d19bd7bcae6
Vladimir Perepelitsa <vladimir.perepelica@onix-systems.com>
2017-04-18 17:57:08 +0300

    1.0.10

---
+++ migrations/m170418_145419_userDevices.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170418_145419_userDevices extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170418_145419_userDevices reverted.\n";
+    }
+}
